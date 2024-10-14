43c5a7941d229884d1a1df6be2d95286dad290b3
Joshieingold <149664064+Joshieingold@users.noreply.github.com>
2024-10-14 14:25:04 -0300

    Made progress bar functional, next is to setup if you make the right move or not to manipulate it

--- src/components/CorrectPercentBar/correctpercentbar.jsx
+++ src/components/CorrectPercentBar/correctpercentbar.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./correctpercentbar.css";
 
 export const CorrectPercentBar = () => {
+    const [correctPercent, setCorrectPercent] = useState(44);
     return (
         <div className="container">
         <div className="progressBar">
-            <div className="progressBarFill">
-
+            <div className="progressBarFill" style={({width: `${correctPercent}%`})}>
+            <div className="progressLabel">{correctPercent}</div>
             </div>
-            <div className="progressLabel"></div>
+            
         </div>
 
     </div>
